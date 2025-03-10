de2490f6be5145cd4871421b0d72a2e02d5776c1
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-03-10 16:14:11 +0500

    add base code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import Konva from 'konva';
-import { useState } from 'react';
+import { Suspense, useState } from 'react';
 import { Layer, Line, Rect, Stage } from 'react-konva';
+import { Link, Route, Routes } from 'react-router';
 import { EditorPanel } from './components/EditorPanel';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 interface Figure {
   id: string;
@@ -77,6 +80,14 @@ function App() {
 
   return (
     <>
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
       <EditorPanel onAddFigure={handleAddFigure} />
       <button
         onClick={handleUndoMove}
