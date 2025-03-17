4482a32d2a9562c2ba318d7c84d94e74ca4f6751
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-03-17 12:06:47 +0500

    add NotFoundPage

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,14 @@
+import classNames from "classnames";
+import cls from "./NotFoundPage.module.scss";
+
+interface INotFoundPageProps {
+    className?: string;
+}
+
+export const NotFoundPage = ({ className }: INotFoundPageProps) => {
+    return (
+        <div className={classNames(cls.NotFoundPage, {}, [className])}>
+            Страница не найдена
+        </div>
+    );
+};
