554bc86709d783f759a2a3450e149c4c1de65a69
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-03-17 12:05:31 +0500

    add ErrorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -0,0 +1,50 @@
+import React, { ErrorInfo, ReactNode } from "react";
+import { PageError } from "widgets/PageError";
+
+// Note
+// Error boundaries do not catch errors for:
+
+// Event handlers (learn more)
+// Asynchronous code (e.g. setTimeout or requestAnimationFrame callbacks)
+// Server side rendering
+// Errors thrown in the error boundary itself (rather than its children)
+
+interface ErrorBoundaryProps {
+    children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+    hasError: boolean;
+}
+
+class ErrorBoundary
+    extends React.Component<ErrorBoundaryProps, ErrorBoundaryState> {
+    constructor(props: ErrorBoundaryProps) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    static getDerivedStateFromError(error: Error) {
+        // Update state so the next render will show the fallback UI.
+        return { hasError: true };
+    }
+
+    componentDidCatch(error: Error, info: ErrorInfo) {
+        // You can also log the error to an error reporting service
+        console.log(error, info);
+    }
+
+    render() {
+        const { hasError } = this.state;
+        const { children } = this.props;
+
+        if (hasError) {
+            // You can render any custom fallback UI
+            return <PageError />;
+        }
+
+        return children;
+    }
+}
+
+export default ErrorBoundary;
---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,20 @@
+import classNames from "classnames";
+import cls from "./PageError.module.scss";
+
+interface IPageErrorProps {
+    className?: string;
+}
+
+export const PageError = ({ className }: IPageErrorProps) => {
+
+    const reloadPage = () => {
+        window.location.reload();
+    };
+
+    return (
+        <div className={classNames(cls.PageError, {}, [className])}>
+            <p>Произошла непредвиденная ошибка</p>
+            <button onClick={reloadPage}>Обновить страницу</button>
+        </div>
+    );
+};
