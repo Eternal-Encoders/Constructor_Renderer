543b85fbfc67e1d1c66197bcd37b5b1664734029
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-03-19 08:18:16 +0500

    add cursor, add actiontypes

--- src/widgets/ObjectPalette/ui/ObjectPalette.tsx
+++ src/widgets/ObjectPalette/ui/ObjectPalette.tsx
@@ -1,18 +1,21 @@
+import Cursor from 'assets/Cursor.svg?react';
 import DottedLine from 'assets/DottedLine.svg?react';
 import Hand from 'assets/Hand.svg?react';
 import Rectangle from 'assets/Rectangle.svg?react';
 import classNames from 'classnames';
+import { ActionType } from 'entities/Figure/Action';
 import { FigureType } from 'entities/Figure/Figure';
 import cls from "./ObjectPalette.module.scss";
 
 interface IObjectPaletteProps {
     selectedFigure: FigureType | null;
+    selectedAction: ActionType;
     setSelectedFigure: (figure: FigureType | null) => void;
+    setSelectedAction: (action: ActionType) => void;
 }
 
-export const ObjectPalette = ({ selectedFigure, setSelectedFigure
-    // onMouseDown, onMouseUp, onMouseLeave 
-}: IObjectPaletteProps) => {
+export const ObjectPalette = ({ selectedFigure, setSelectedFigure, selectedAction, setSelectedAction }
+    : IObjectPaletteProps) => {
 
     const onMouseDown = (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
         event.preventDefault();
@@ -27,7 +30,9 @@ export const ObjectPalette = ({ selectedFigure, setSelectedFigure
                         onMouseDown(event);
                         return setSelectedFigure(FigureType.Rectangle);
                     }}
-                    className={classNames(cls.ObjectPalette__item, selectedFigure === FigureType.Rectangle ? cls.ObjectPalette__item_active : undefined)}
+                    className={classNames(cls.ObjectPalette__item, selectedFigure === FigureType.Rectangle
+                        ? cls.ObjectPalette__item_active
+                        : undefined)}
                     onClick={() => setSelectedFigure(FigureType.Rectangle)}>
                     <Rectangle />
                 </li>
@@ -36,17 +41,32 @@ export const ObjectPalette = ({ selectedFigure, setSelectedFigure
                         onMouseDown(event);
                         return setSelectedFigure(FigureType.DottedLine);
                     }}
-                    className={classNames(cls.ObjectPalette__item, selectedFigure === FigureType.DottedLine ? cls.ObjectPalette__item_active : undefined)}
+                    className={classNames(cls.ObjectPalette__item, selectedFigure === FigureType.DottedLine
+                        ? cls.ObjectPalette__item_active
+                        : undefined)}
                     onClick={() => setSelectedFigure(FigureType.DottedLine)}>
                     <DottedLine />
                 </li>
                 <li
                     onMouseDown={(event) => {
                         onMouseDown(event);
-                        return setSelectedFigure(null);
+                        return setSelectedAction(ActionType.Cursor);
                     }}
-                    className={classNames(cls.ObjectPalette__item, selectedFigure === null ? cls.ObjectPalette__item_active : undefined)}
-                    onClick={() => setSelectedFigure(null)}>
+                    className={classNames(cls.ObjectPalette__item, selectedAction === ActionType.Cursor
+                        ? cls.ObjectPalette__item_active
+                        : undefined)}
+                    onClick={() => setSelectedAction(ActionType.Cursor)}>
+                    <Cursor />
+                </li>
+                <li
+                    onMouseDown={(event) => {
+                        onMouseDown(event);
+                        return setSelectedAction(ActionType.Drag);
+                    }}
+                    className={classNames(cls.ObjectPalette__item, selectedAction === ActionType.Drag
+                        ? cls.ObjectPalette__item_active
+                        : undefined)}
+                    onClick={() => setSelectedAction(ActionType.Drag)}>
                     <Hand />
                 </li>
             </ul>
