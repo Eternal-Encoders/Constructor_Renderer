95221ed4590354a59f2148439b4f789add50203a
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-03-19 08:17:03 +0500

    improve select

--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -3,27 +3,33 @@ import cls from "./Select.module.scss";
 
 interface ISelectProps {
     className?: string;
-    scale: number | null;
+    scale: number;
     setScale: (props: number) => void;
 }
 
 export const Select = ({ className, setScale, scale }: ISelectProps) => {
-    if (scale === null) scale = 1;
+    const predefinedScales = [0.1, 0.5, 0.8, 1, 2, 3];
+    const uniqueScales = predefinedScales.includes(scale) ? predefinedScales : [...predefinedScales, scale];
 
     return (
         <div className={classNames(cls.Select, {}, [className])}>
             <label className={classNames(cls.Select__label)}>
                 Масштаб:
-                <select value={scale.toString()} name="selectedScale" id="selectedScale" onChange={(e) => setScale(parseFloat(e.target.value))}>
-                    <option value="0.1">0.1</option>
-                    <option value="0.5">0.5</option>
-                    <option value="0.8">0.8</option>
-                    <option value="1">1 (Обычный)</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value={scale.toString()}>Польз: {scale.toFixed(1).toString()}</option>
+                <select
+                    value={scale.toString()}
+                    name="selectedScale"
+                    id="selectedScale"
+                    onChange={(e) => setScale(parseFloat(e.target.value))}
+                >
+                    {uniqueScales.map((s) => (
+                        <option key={s.toString()} value={s.toString()}>
+                            {s === scale && !predefinedScales.includes(s) ? `Польз: ${s.toFixed(1)}` : s}
+                        </option>
+                    ))}
                 </select>
             </label>
         </div>
     );
+
 };
+
