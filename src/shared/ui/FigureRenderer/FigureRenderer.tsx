baa23a788b8826ccccc310f59ae4e627f59786be
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-03-27 14:24:04 +0500

    magnetic polygons and rectangles

--- src/shared/ui/FigureRenderer/FigureRenderer.tsx
+++ src/shared/ui/FigureRenderer/FigureRenderer.tsx
@@ -1,7 +1,10 @@
 import { ActionType } from "entities/Figure/Action";
-import { Figure, Polygon, Rectangle } from "entities/Figure/Figure";
+import { Figure, FigureType, Polygon, Rectangle } from "entities/Figure/Figure";
 import Konva from "konva";
 import { KonvaEventObject } from "konva/lib/Node";
+import { Shape, ShapeConfig } from "konva/lib/Shape";
+import { Stage } from "konva/lib/Stage";
+import { IRect } from "konva/lib/types";
 import { useEffect, useRef, useState } from "react";
 import { Line, Rect, Transformer } from "react-konva";
 
@@ -13,26 +16,16 @@ interface IFigureRendererProps {
     setPolygons: (rectangles: Polygon[] | ((prev: Polygon[]) => Polygon[])) => void;
     selectedAction: ActionType;
     setSelectedId: (id: string | null) => void;
+    layerRef: React.RefObject<Konva.Layer | null>;
 }
 
 const MAGNETIC_DISTANCE = 10;
 
-export const FigureRenderer = ({ className, figure, selectedId, selectedAction, setSelectedId, setRectangles, setPolygons }: IFigureRendererProps) => {
+export const FigureRenderer = ({ className, figure, selectedId, selectedAction, setSelectedId, setRectangles, setPolygons, layerRef, }: IFigureRendererProps) => {
     const transformerRef = useRef<Konva.Transformer>(null);
     const selectedNodeRef = useRef<Konva.Node | null>(null);
 
     const [isHovered, setIsHovered] = useState(false);
-
-    const onDragStart = (e: KonvaEventObject<DragEvent>) => {
-        const node = e.target;
-        setRectangles((prev) =>
-            prev.map((fig) =>
-                fig.id === node.id()
-                    ? { ...fig, history: [...fig.history, { x: fig.x, y: fig.y }] }
-                    : fig
-            )
-        );
-    };
 
     useEffect(() => {
         if (transformerRef.current) {
@@ -49,31 +42,34 @@ export const FigureRenderer = ({ className, figure, selectedId, selectedAction, 
         }
     }, [selectedId]);
 
-    const onDragMove = (e: KonvaEventObject<DragEvent>) => {
-        if (selectedAction !== ActionType.Cursor) return;
+    const onDragStart = (e: KonvaEventObject<DragEvent>) => {
         const node = e.target;
-        const nodePos = node.position();
+        setRectangles((prev) =>
+            prev.map((fig) =>
+                fig.id === node.id()
+                    ? { ...fig, history: [...fig.history, { x: fig.x, y: fig.y }] }
+                    : fig
+            )
+        );
+    };
 
-        let newX = nodePos.x;
-        let newY = nodePos.y;
+    const doMagneticForRectangle = (node: Shape<ShapeConfig> | Stage,
+        nodeBox: IRect, newY: number,
+        newX: number, borderOffset: number, fig: Rectangle) => {
+        const figRight = fig.x + (fig.width || 0);
+        const figBottom = fig.y + (fig.height || 0);
 
-        figure.rectangles.forEach(fig => {
-            if (fig.id === node.id()) return;
-            const figRight = fig.x + (fig.width || 0);
-            const figBottom = fig.y + (fig.height || 0);
+        if (Math.abs(newX - figRight) <= MAGNETIC_DISTANCE) {
+            newX = figRight;
+        } else if (Math.abs(fig.x - (newX + (nodeBox.width - borderOffset || 0))) <= MAGNETIC_DISTANCE) {
+            newX = fig.x - (nodeBox.width - borderOffset || 0);
+        }
 
-            if (Math.abs(newX - figRight) <= MAGNETIC_DISTANCE) {
-                newX = figRight;
-            } else if (Math.abs(fig.x - (newX + (node.width() || 0))) <= MAGNETIC_DISTANCE) {
-                newX = fig.x - (node.width() || 0);
-            }
-
-            if (Math.abs(newY - figBottom) <= MAGNETIC_DISTANCE) {
-                newY = figBottom;
-            } else if (Math.abs(fig.y - (newY + (node.height() || 0))) <= MAGNETIC_DISTANCE) {
-                newY = fig.y - (node.height() || 0);
-            }
-        });
+        if (Math.abs(newY - figBottom) <= MAGNETIC_DISTANCE) {
+            newY = figBottom;
+        } else if (Math.abs(fig.y - (newY + (nodeBox.height - borderOffset || 0))) <= MAGNETIC_DISTANCE) {
+            newY = fig.y - (nodeBox.height - borderOffset || 0);
+        }
 
         node.position({ x: newX, y: newY });
         setRectangles((prev) =>
@@ -83,25 +79,147 @@ export const FigureRenderer = ({ className, figure, selectedId, selectedAction, 
         );
     };
 
+    const doMagneticForPolygonRectangle = (node: Shape<ShapeConfig> | Stage,
+        nodeBox: IRect, newY: number,
+        newX: number, borderOffset: number, fig: Rectangle | Polygon) => {
+        if (!layerRef.current) return;
+        const figNodeBox = layerRef.current.findOne(`#${fig.id}`);
+        if (!figNodeBox) return;
+        const polNodeBox = figNodeBox?.getClientRect();
+
+        const figRight = polNodeBox.x + (polNodeBox.width || 0);
+        const figBottom = polNodeBox.y + (polNodeBox.height || 0);
+
+
+        if (Math.abs(newX - figRight) <= MAGNETIC_DISTANCE) {
+            newX = figRight;
+        } else if (Math.abs(nodeBox.x + nodeBox.width - polNodeBox.x) <= MAGNETIC_DISTANCE) {
+            newX += Math.abs(nodeBox.x + nodeBox.width - polNodeBox.x) + borderOffset;
+        }
+
+        if (Math.abs(newY - figBottom) <= MAGNETIC_DISTANCE) {
+            newY = figBottom;
+        } else if (Math.abs(fig.y - (newY + (nodeBox.height - borderOffset || 0))) <= MAGNETIC_DISTANCE) {
+            newY += Math.abs(nodeBox.y + nodeBox.height - polNodeBox.y) + borderOffset;
+        }
+
+        node.position({ x: newX, y: newY });
+        setRectangles((prev) =>
+            prev.map((fig) =>
+                fig.id === node.id() ? { ...fig, x: newX, y: newY } : fig
+            )
+        );
+    };
+
+    const doMagneticForPolygon = (node: Shape<ShapeConfig> | Stage,
+        nodeBox: IRect, newY: number,
+        newX: number, borderOffset: number, fig: Rectangle | Polygon) => {
+        if (!layerRef.current) return;
+        newY = node.y();
+        newX = node.x();
+        const figNodeBox = layerRef.current.findOne(`#${fig.id}`);
+        if (!figNodeBox) return;
+        const rectNodeBox = figNodeBox?.getClientRect();
+
+        const figRight = rectNodeBox.x + (rectNodeBox.width || 0);
+        const figBottom = rectNodeBox.y + (rectNodeBox.height || 0);
+
+        if (Math.abs(nodeBox.x + nodeBox.width - rectNodeBox.x) <= MAGNETIC_DISTANCE) {
+            newX = node.x() + Math.abs(nodeBox.x + nodeBox.width - rectNodeBox.x) + borderOffset;
+        } else if (Math.abs(nodeBox.x - figRight) <= MAGNETIC_DISTANCE) {
+            newX = node.x() - Math.abs(nodeBox.x - figRight) - borderOffset;
+        }
+
+        if (Math.abs(nodeBox.y - figBottom) <= MAGNETIC_DISTANCE) {
+            newY = node.y() - Math.abs(nodeBox.y - figBottom) - borderOffset;
+        }
+        else if (Math.abs(nodeBox.y + nodeBox.height - rectNodeBox.y) <= MAGNETIC_DISTANCE) {
+            newY = node.y() + Math.abs(nodeBox.y + nodeBox.height - rectNodeBox.y) + borderOffset;
+        }
+
+        node.position({ x: newX, y: newY });
+        setPolygons((prev) =>
+            prev.map((fig) =>
+                fig.id === node.id() ? { ...fig, x: newX, y: newY } : fig
+            )
+        );
+    };
+
+    const onDragMove = (e: KonvaEventObject<DragEvent>) => {
+        if (selectedAction !== ActionType.Cursor) return;
+
+        const allFigures = [...figure.rectangles, ...figure.polygons];
+
+        const borderOffset = 3;
+        const node = e.target;
+        const nodeBox = node.getClientRect();
+        const newX = nodeBox.x;
+        const newY = nodeBox.y;
+
+        allFigures.forEach(fig => {
+            if (fig.id === node.id()) return;
+            switch (fig.type) {
+                case (FigureType.Rectangle):
+                    switch (node.attrs.type) {
+                        case (FigureType.Rectangle):
+                            doMagneticForRectangle(node, nodeBox, newY, newX, borderOffset, fig);
+                            break;
+                        case (FigureType.Polygon):
+                            doMagneticForPolygon(node, nodeBox, newY, newX, borderOffset, fig);
+                            break;
+                    }
+                    break;
+                case (FigureType.Polygon):
+                    switch (node.attrs.type) {
+                        case (FigureType.Rectangle):
+                            doMagneticForPolygonRectangle(node, nodeBox, newY, newX, borderOffset, fig);
+                            break;
+                        case (FigureType.Polygon):
+                            doMagneticForPolygon(node, nodeBox, newY, newX, borderOffset, fig);
+                            break;
+                    }
+                    break;
+            }
+        });
+    };
+
     const onTransformEnd = (e: KonvaEventObject<Event>) => {
         const node = e.target;
 
         const newWidth = node.width() * node.scaleX();
         const newHeight = node.height() * node.scaleY();
 
-        setRectangles((prev) =>
-            prev.map((fig) =>
-                fig.id === node.id()
-                    ? {
-                        ...fig,
-                        x: node.x(),
-                        y: node.y(),
-                        width: newWidth,
-                        height: newHeight,
-                    }
-                    : fig
-            )
-        );
+        // Проверка на прямоугольник, т.к. у него type - rectangle, а у мног-ка undefined
+        if (node.attrs.type === FigureType.Rectangle) {
+            setRectangles((prev) =>
+                prev.map((fig) =>
+                    fig.id === node.id()
+                        ? {
+                            ...fig,
+                            x: node.x(),
+                            y: node.y(),
+                            width: newWidth,
+                            height: newHeight,
+                        }
+                        : fig
+                )
+            );
+        } else if (node.attrs.type === FigureType.Polygon) {
+            setPolygons((prev) =>
+                prev.map((fig) =>
+                    fig.id === node.id()
+                        ? {
+                            ...fig,
+                            x: node.x(),
+                            y: node.y(),
+                            width: newWidth,
+                            height: newHeight,
+                        }
+                        : fig
+                )
+            );
+        }
+
 
         // Сбрасываем scale после обновления стейта
         node.scaleX(1);
@@ -117,6 +235,8 @@ export const FigureRenderer = ({ className, figure, selectedId, selectedAction, 
                     onDragStart={onDragStart}
                     key={fig.id}
                     {...fig}
+                    id={fig.id}
+                    type={fig.type}
                     fill={(selectedId === fig.id) ? "lightblue" : "#00D2FF"}
                     stroke="black"
                     strokeWidth={1}
@@ -124,6 +244,7 @@ export const FigureRenderer = ({ className, figure, selectedId, selectedAction, 
                     draggable={selectedAction === ActionType.Cursor}
                     onDragMove={onDragMove}
                     onClick={(e) => {
+                        console.log(e);
                         if (selectedAction !== ActionType.Cursor) return;
                         e.cancelBubble = true;
                         setSelectedId(fig.id);
@@ -137,33 +258,37 @@ export const FigureRenderer = ({ className, figure, selectedId, selectedAction, 
                     }}
                 />
             ))}
-            {figure.polygons.map((pol) => (
-                <Line
-                    onMouseEnter={() => setIsHovered(true)}
-                    onMouseLeave={() => setIsHovered(false)}
-                    onDragStart={onDragStart}
-                    key={pol.id}
-                    points={pol.points}
-                    stroke="black"
-                    strokeWidth={1}
-                    shadowBlur={1}
-                    closed={pol.isClosed}
-                    fill={pol.isClosed ? "rgba(0,0,255,0.3)" : "transparent"}
-                    draggable={selectedAction === ActionType.Cursor}
-                    onDragMove={onDragMove}
-                    onClick={(e) => {
-                        if (selectedAction !== ActionType.Cursor) return;
-                        e.cancelBubble = true;
-                        setSelectedId(pol.id);
-                        selectedNodeRef.current = e.target;
-                    }}
-                    ref={(node) => {
-                        if (selectedId === pol.id) {
-                            selectedNodeRef.current = node;
-                        }
-                    }}
-                />
-            ))}
+            {figure.polygons.map((pol) => {
+                return (
+                    <Line
+                        onMouseEnter={() => setIsHovered(true)}
+                        onMouseLeave={() => setIsHovered(false)}
+                        onDragStart={onDragStart}
+                        key={pol.id}
+                        id={pol.id}
+                        type={pol.type}
+                        points={pol.points}
+                        stroke="black"
+                        strokeWidth={1}
+                        shadowBlur={1}
+                        closed={pol.isClosed}
+                        fill={pol.isClosed ? "rgba(0,0,255,0.3)" : "transparent"}
+                        draggable={selectedAction === ActionType.Cursor}
+                        onDragMove={onDragMove}
+                        onClick={(e) => {
+                            if (selectedAction !== ActionType.Cursor) return;
+                            e.cancelBubble = true;
+                            setSelectedId(pol.id);
+                            selectedNodeRef.current = e.target;
+                        }}
+                        ref={(node) => {
+                            if (selectedId === pol.id) {
+                                selectedNodeRef.current = node;
+                            }
+                        }}
+                    />
+                );
+            })}
             <Transformer
                 ref={transformerRef}
                 boundBoxFunc={(oldBox, newBox) => {
