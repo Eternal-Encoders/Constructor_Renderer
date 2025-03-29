de75eadd842100660fc33d2c1c5a6ec1f8de291d
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-03-29 19:48:41 +0500

    add func getRelativePointerPosition

---
+++ src/helpers/getRelativePointerPosition.tsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getRelativePointerPosition = (stage: any, scale: number) => {
+    const pointer = stage.getPointerPosition();
+    if (!pointer) return null;
+
+    const stagePos = stage.position();
+    return {
+        x: (pointer.x - stagePos.x) / scale,
+        y: (pointer.y - stagePos.y) / scale
+    };
+};
