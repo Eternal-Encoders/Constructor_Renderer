4332b4531017149958ccb128f3e71192d54b3e9d
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-04-01 05:44:59 +0500

    improve hooks

--- src/helpers/hooks/useKeyboardShortcuts.tsx
+++ src/helpers/hooks/useKeyboardShortcuts.tsx
@@ -4,60 +4,61 @@ import { useEffect, useRef } from "react";
 
 
 interface UseKeyboardShortcutsProps {
-    selectedId: string | null;
-    handleDelete: () => void;
-    setSelectedAction: (action: ActionType) => void;
-    setSelectedFigure: (figure: FigureType) => void;
+  selectedId: string | null;
+  handleDelete: () => void;
+  setSelectedAction: (action: ActionType) => void;
+  setSelectedFigure: (figure: FigureType) => void;
 }
 
 export function useKeyboardShortcuts({
-    selectedId,
-    handleDelete,
-    setSelectedAction,
-    setSelectedFigure,
+  selectedId,
+  handleDelete,
+  setSelectedAction,
+  setSelectedFigure,
 }: UseKeyboardShortcutsProps) {
-    const keyDownHandlerRef = useRef<((event: KeyboardEvent) => void) | null>(null);
+  const keyDownHandlerRef = useRef<((event: KeyboardEvent) => void) | null>(null);
 
-    useEffect(() => {
-        keyDownHandlerRef.current = (event: KeyboardEvent) => {
-            switch (event.code) {
-                case "Delete":
-                    if (selectedId) handleDelete();
-                    break;
-                case "KeyV":
-                    setSelectedAction(ActionType.Cursor);
-                    setSelectedFigure(FigureType.None);
-                    break;
-                case "KeyR":
-                    setSelectedFigure(FigureType.Rectangle);
-                    setSelectedAction(ActionType.None);
-                    break;
-                case "KeyP":
-                    setSelectedFigure(FigureType.Polygon);
-                    setSelectedAction(ActionType.None);
-                    break;
-                case "Space":
-                    event.preventDefault();
-                    setSelectedAction(ActionType.Drag);
-                    setSelectedFigure(FigureType.None);
-                    break;
-                default:
-                    break;
-            }
-        };
-    }, [selectedId, handleDelete, setSelectedAction, setSelectedFigure]);
+  useEffect(() => {
+    keyDownHandlerRef.current = (event: KeyboardEvent) => {
+      switch (event.code) {
+        case "Delete":
+          if (selectedId) handleDelete();
+          break;
+        case "KeyV":
+          setSelectedAction(ActionType.Cursor);
+          setSelectedFigure(FigureType.None);
+          break;
+        case "KeyR":
+          setSelectedFigure(FigureType.Rectangle);
+          setSelectedAction(ActionType.None);
+          break;
+        case "KeyP":
+          setSelectedFigure(FigureType.Polygon);
+          setSelectedAction(ActionType.None);
+          break;
+        case "Space":
+          if (document.activeElement !== document.body) return;  
+          event.preventDefault();
+          setSelectedAction(ActionType.Drag);
+          setSelectedFigure(FigureType.None);
+          break;
+        default:
+          break;
+      }
+    };
+  }, [selectedId, handleDelete, setSelectedAction, setSelectedFigure]);
 
-    useEffect(() => {
-        const handleKeyDown = (event: KeyboardEvent) => {
-            if (keyDownHandlerRef.current) {
-                keyDownHandlerRef.current(event);
-            }
-        };
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (keyDownHandlerRef.current) {
+        keyDownHandlerRef.current(event);
+      }
+    };
 
-        window.addEventListener("keydown", handleKeyDown);
-        return () => {
-            window.removeEventListener("keydown", handleKeyDown);
-        };
-    }, []); // useEffect вызывается только один раз при монтировании
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []); // useEffect вызывается только один раз при монтировании
 
 }
--- src/helpers/hooks/useMiddleMouseHold.tsx
+++ src/helpers/hooks/useMiddleMouseHold.tsx
@@ -5,17 +5,17 @@ import { useEffect } from "react";
 type MouseHandler = (action: ActionType, figure: FigureType) => void;
 
 export function useMiddleMouseHold(setState: MouseHandler) {
-    useEffect(() => {
-        const handleMouseDown = (event: MouseEvent) => {
-            if (event.button === 1) { // Средняя кнопка мыши
-                event.preventDefault();
-                setState(ActionType.Drag, FigureType.None);
-            }
-        };
+  useEffect(() => {
+    const handleMouseDown = (event: MouseEvent) => {
+      if (event.button === 1) { // Средняя кнопка мыши
+        event.preventDefault();
+        setState(ActionType.Drag, FigureType.None);
+      }
+    };
 
-        window.addEventListener("mousedown", handleMouseDown);
-        return () => {
-            window.removeEventListener("mousedown", handleMouseDown);
-        };
-    }, [setState]);
+    window.addEventListener("mousedown", handleMouseDown);
+    return () => {
+      window.removeEventListener("mousedown", handleMouseDown);
+    };
+  }, [setState]);
 }
