356a5a7d309773c20363a869d2f97b43f8ec0a68
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-04-23 20:22:36 +0500

    add styles for storybook

--- src/shared/config/storybook/main.ts
+++ src/shared/config/storybook/main.ts
@@ -1,4 +1,6 @@
 import type { StorybookConfig } from '@storybook/react-vite';
+import path from 'path';
+import { mergeConfig } from 'vite';
 
 const config: StorybookConfig = {
   "stories": [
@@ -14,6 +16,24 @@ const config: StorybookConfig = {
   "framework": {
     "name": "@storybook/react-vite",
     "options": {}
-  }
+  },
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      resolve: {
+        alias: {
+          App: path.resolve(__dirname, '../../../App.tsx'),
+          app: path.resolve(__dirname, '../../../app'),
+          assets: path.resolve(__dirname, '../../../assets'),
+          entities: path.resolve(__dirname, '../../../entities'),
+          helpers: path.resolve(__dirname, '../../../helpers'),
+          pages: path.resolve(__dirname, '../../../pages'),
+          processes: path.resolve(__dirname, '../../../processes'),
+          shared: path.resolve(__dirname, '../../../shared'),
+          widgets: path.resolve(__dirname, '../../../widgets'),
+          features: path.resolve(__dirname, '../../../features'),
+        },
+      },
+    });
+  },
 };
 export default config;
