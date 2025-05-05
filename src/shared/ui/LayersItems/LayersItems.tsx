5e2bd5b9b2705ed4468da24424312161cb89ba04
Daniil Mukhortikov <113042616+DanRaySS@users.noreply.github.com>
2025-05-05 19:39:20 +0500

    delete unused code

--- src/shared/ui/LayersItems/LayersItems.tsx
+++ src/shared/ui/LayersItems/LayersItems.tsx
@@ -10,8 +10,7 @@ interface ILayersItemsProps {
 
 export const LayersItems = ({ className }: ILayersItemsProps) => {
   const figures = useSelector(getLayersFigures);
-
-  // const isActive = true;
+  
   return (
     <ul className={classNames(cls.LayersItems, {}, [className])}>
       {figures && figures.map((layer) => (
@@ -22,18 +21,6 @@ export const LayersItems = ({ className }: ILayersItemsProps) => {
           <h6 className={classNames(cls.LayersItems__title)}>{layer.type}</h6>
         </li>
       ))}
-      {/* <li className={classNames(cls.LayersItems__item)}>
-        <div className={classNames(cls.LayersItems__leftIcon)}>
-          <Prison/>
-        </div>
-        <h6 className={classNames(cls.LayersItems__title)}>Дверь 1</h6>
-      </li>
-      <li className={classNames(cls.LayersItems__item, isActive && cls.LayersItems__item_active)}>
-        <div className={classNames(cls.LayersItems__leftIcon)}>
-          <Prison/>
-        </div>
-        <h6 className={classNames(cls.LayersItems__title)}>Комната 2</h6>
-      </li> */}
     </ul>
   );
 };
